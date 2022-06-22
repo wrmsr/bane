a0f43d1c1769ba83d361c8d524da50fbbf4fe650
wrmsr <timwilloney@gmail.com>
2022-06-22 14:52:14 -0700

    --

---
+++ pkg/util/os/env.go
@@ -0,0 +1,43 @@
+package os
+
+import (
+	"os"
+
+	"github.com/wrmsr/bane/pkg/util/check"
+	"github.com/wrmsr/bane/pkg/util/maps"
+	opt "github.com/wrmsr/bane/pkg/util/optional"
+)
+
+func GetEnvs(ks []string) map[string]opt.Optional[string] {
+	m := make(map[string]opt.Optional[string], len(ks))
+	for _, k := range ks {
+		if ov, ok := os.LookupEnv(k); ok {
+			m[k] = opt.Just(ov)
+		} else {
+			m[k] = opt.None[string]()
+		}
+	}
+	return m
+}
+
+func SetEnvs(m map[string]opt.Optional[string]) {
+	for k, v := range m {
+		if v.Present() {
+			check.NoErr(os.Setenv(k, v.Value()))
+		} else {
+			check.NoErr(os.Unsetenv(k))
+		}
+	}
+}
+
+func SwapEnvs(m map[string]opt.Optional[string]) func() {
+	if m == nil || len(m) < 1 {
+		return func() {}
+	}
+
+	o := GetEnvs(maps.Keys(m))
+	SetEnvs(m)
+	return func() {
+		SetEnvs(o)
+	}
+}
