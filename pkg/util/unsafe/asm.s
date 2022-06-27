7260509c3f271f8d788caec69b5567976904ddba
wrmsr <timwilloney@gmail.com>
2022-06-27 12:08:50 -0700

    --

--- pkg/util/unsafe/asm.s
+++ pkg/util/unsafe/asm.s
@@ -1,6 +1,9 @@
-#include "go_asm.h"
+#include "funcdata.h"
 #include "textflag.h"
 
-TEXT ·__addints(SB),NOSPLIT,$0-24
-    ADD R1, R0, 16(SB)
-	MOVD	$ret+32(FP), R8
+TEXT ·__addints(SB),NOSPLIT|NOFRAME,$0-24
+    MOVD x(FP), R0
+    MOVD y+8(FP), R1
+	ADD R1, R0, R0
+	MOVD R0, ret+16(FP)
+	RET (R30)
