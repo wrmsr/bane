1f5891c868597fb493eb09ce0bc162c9d33d2424
wrmsr <timwilloney@gmail.com>
2022-06-30 19:15:42 -0700

    --

--- pkg/util/marshal/structs.go
+++ pkg/util/marshal/structs.go
@@ -1,5 +1,23 @@
 package marshal
 
-type StructFragment struct {
-	M map[string]any
+import (
+	"reflect"
+
+	stu "github.com/wrmsr/bane/pkg/util/structs"
+)
+
+//
+
+type StructMarshaler struct {
+	si *stu.StructInfo
+}
+
+func NewStructMarshaler(si *stu.StructInfo) StructMarshaler {
+	return StructMarshaler{si: si}
+}
+
+var _ Marshaler = StructMarshaler{}
+
+func (s StructMarshaler) Marshal(ctx MarshalContext, rv reflect.Value) (Value, error) {
+	panic("implement me")
 }
