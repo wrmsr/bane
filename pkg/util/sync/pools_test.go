d9888b1ebff189e895dda85ccf525f277a2e85d4
wrmsr <timwilloney@gmail.com>
2022-07-02 12:07:56 -0700

    --

--- pkg/util/sync/pools_test.go
+++ pkg/util/sync/pools_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	tu "github.com/wrmsr/bane/pkg/util/dev/testing"
 	rfl "github.com/wrmsr/bane/pkg/util/reflect"
 )
 
@@ -16,14 +17,17 @@ func TestPool(t *testing.T) {
 	xs := make([]*int, 10)
 	for i := range xs {
 		xs[i] = p.Get()
+		tu.AssertEqual(t, len(p.m), i+1)
 		*xs[i] = 420 + i
 	}
 	for i := range xs {
 		p.Put(xs[i])
 	}
+	tu.AssertEqual(t, len(p.m), len(xs))
 
 	for i := 0; i < len(xs)+3; i++ {
 		x := p.Get()
 		fmt.Printf("%d %x %d\n", i, x, *x)
 	}
+	tu.AssertEqual(t, len(p.m), len(xs)+3)
 }
