ef6e5627346510a488748d3a67003e8bdadac9a2
wrmsr <timwilloney@gmail.com>
2022-07-05 18:28:47 -0700

    --

--- pkg/util/marshal/registry.go
+++ pkg/util/marshal/registry.go
@@ -1,1 +1,42 @@
 package marshal
+
+import (
+	"reflect"
+
+	ctr "github.com/wrmsr/bane/pkg/util/container"
+)
+
+type RegistryItem interface {
+	isRegistryItem()
+}
+
+type typeRegistry struct {
+	ty reflect.Type
+	m  ctr.MutTypeMap[RegistryItem]
+}
+
+type Registry struct {
+	m  map[reflect.Type]*typeRegistry
+	ps []*Registry
+}
+
+func NewRegistry(parents []*Registry) *Registry {
+	return &Registry{
+		m: make(map[reflect.Type]*typeRegistry),
+
+		ps: parents,
+	}
+}
+
+func (r *Registry) Register(ty reflect.Type, item RegistryItem) *Registry {
+	ti := r.m[ty]
+	if ti == nil {
+		ti = &typeRegistry{
+			ty: ty,
+			m:  ctr.NewMutTypeMap[RegistryItem](nil),
+		}
+		r.m[ty] = ti
+	}
+
+	return r
+}
