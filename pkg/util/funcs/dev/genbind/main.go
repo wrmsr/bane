0521543ea061527cf808165a9fb5c62db5a96828
wrmsr <timwilloney@gmail.com>
2022-07-07 10:54:08 -0700

    --

--- pkg/util/funcs/dev/genbind/main.go
+++ pkg/util/funcs/dev/genbind/main.go
@@ -1,29 +1,10 @@
 //go:build !nodev
 
-/*
-.a_ab
-.a_ba
-.a_bar
-.a_p
-
-.n_a
-.n_b
-.n_r
-
-.p_a
-.p_b
-
-.pt_r
-
-.t_a
-.t_ab
-.t_abr
-.t_ba
-.t_p
-*/
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -35,18 +16,74 @@ import (
 	its "github.com/wrmsr/bane/pkg/util/iterators"
 )
 
-func main() {
-	if len(os.Args) != 4 {
-		panic("invalid args")
+type dim struct {
+	name string
+	num  int
+
+	types  []string
+	params []string
+	args   []string
+}
+
+func newDim(name string, num int) dim {
+	return dim{
+		name: name,
+		num:  num,
+
+		types:  its.Seq(its.Map(its.Range1(0, num), func(i int) string { return fmt.Sprintf("%s%d", strings.ToUpper(name), i) })),
+		params: its.Seq(its.Map(its.Range1(0, num), func(i int) string { return fmt.Sprintf("%s%d %s%d", name, i, strings.ToUpper(name), i) })),
+		args:   its.Seq(its.Map(its.Range1(0, num), func(i int) string { return fmt.Sprintf("%s%d", name, i) })),
+	}
+}
+
+func joinDims(ds ...dim) dim {
+	var j dim
+	for _, d := range ds {
+		j.name += d.name
+		j.num += d.num
+
+		j.types = append(j.types, d.types...)
+		j.params = append(j.params, d.params...)
+		j.args = append(j.args, d.args...)
+	}
+	return j
+}
+
+func (d dim) load(m map[string]any) {
+	m["n_"+d.name] = strconv.Itoa(d.num)
+
+	t := strings.Join(d.types, ", ")
+
+	m["t_"+d.name] = t
+	if d.num > 1 {
+		m["pt_"+d.name] = "(" + t + ")"
+	} else {
+		m["pt_"+d.name] = t
 	}
 
-	bn := check.Must1(strconv.Atoi(os.Args[1]))
-	an := check.Must1(strconv.Atoi(os.Args[2]))
-	rn := check.Must1(strconv.Atoi(os.Args[3]))
+	m["p_"+d.name] = strings.Join(d.params, ", ")
+
+	m["a_"+d.name] = strings.Join(d.args, ", ")
+}
+
+func main() {
+	flags := flag.NewFlagSet("gen", flag.ExitOnError)
+
+	var noWrite bool
+	flags.BoolVar(&noWrite, "p", false, "do not write file, just print")
+
+	check.Must(flags.Parse(os.Args[1:]))
+	if flags.NArg() != 3 {
+		panic(errors.New("expected 3 args"))
+	}
+
+	bn := check.Must1(strconv.Atoi(flags.Args()[0]))
+	an := check.Must1(strconv.Atoi(flags.Args()[1]))
+	rn := check.Must1(strconv.Atoi(flags.Args()[2]))
 
 	tmpl := check.Must1(template.New("?").Parse(`
 func Bind{{- .n_b -}}x{{- .n_a -}}x{{- .n_r -}}[{{.a_bar}} any](fn func({{.t_ba}}) {{.pr_r}}, {{.p_b}}) func({{.t_a}}) {{.pt_r}} {
-	return func({{.a_p}}) {{.pt_r}} {
+	return func({{.p_a}}) {{.pt_r}} {
 		return fn({{.a_ba}})
 	}
 }
@@ -58,38 +95,37 @@ func BindR{{- .n_a -}}x{{- .n_b -}}x{{- .n_r -}}[{{.t_abr}} any](fn func({{.t_ab
 }
 `))
 
-	pop := func(m map[string]string, p string, n int) {
-		ts := its.Seq(its.Map(its.Range1(0, n), func(i int) string { return fmt.Sprintf("%s%d", strings.ToUpper(p), i) }))
-		ps := its.Seq(its.Map(its.Range1(0, n), func(i int) string { return fmt.Sprintf("%s%d %s%d", p, i, strings.ToUpper(p), i) }))
-		as := its.Seq(its.Map(its.Range1(0, n), func(i int) string { return fmt.Sprintf("%s%d", p, i) }))
-		m[p+"n"] = strconv.Itoa(n)
-		m[p+"ts"] = tsb.String()
-		m[p+"ps"] = psb.String()
-		m[p+"as"] = asb.String()
-		if p == "r" {
-			if n > 1 {
-				m["prts"] = "(" + tsb.String() + ")"
-			} else {
-				m["prts"] = tsb.String()
-			}
-		}
-	}
-
 	var out strings.Builder
 	out.WriteString("package funcs\n")
 
-	for r := 1; r < rn+1; r++ {
-		for a := 1; a < an+1; a++ {
-			for b := 1; b < bn+1; b++ {
-				m := make(map[string]string)
-				pop(m, "b", b)
-				pop(m, "a", a)
-				pop(m, "r", r)
+	for cr := 1; cr < rn+1; cr++ {
+		for ca := 1; ca < an+1; ca++ {
+			for cb := 1; cb < bn+1; cb++ {
+				b := newDim("b", cb)
+				a := newDim("a", ca)
+				r := newDim("r", cr)
+
+				m := make(map[string]any)
+				for _, ds := range [][]dim{
+					{a},
+					{b},
+					{r},
+					{a, b},
+					{b, a},
+					{a, b, r},
+					{b, a, r},
+				} {
+					joinDims(ds...).load(m)
+				}
 
 				check.Must(tmpl.Execute(&out, m))
 			}
 		}
 	}
 
-	check.Must(ioutil.WriteFile("bind_gen.go", []byte(out.String()), 0644))
+	if noWrite {
+		fmt.Println(out.String())
+	} else {
+		check.Must(ioutil.WriteFile("bind_gen.go", []byte(out.String()), 0644))
+	}
 }
