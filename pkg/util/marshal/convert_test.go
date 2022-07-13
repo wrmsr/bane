b02fb8868f6cf20a3a1303252067a9c2ed702dd5
wrmsr <timwilloney@gmail.com>
2022-07-12 17:09:01 -0700

    --

--- pkg/util/marshal/convert_test.go
+++ pkg/util/marshal/convert_test.go
@@ -6,17 +6,11 @@ import (
 
 	"github.com/wrmsr/bane/pkg/util/check"
 	tu "github.com/wrmsr/bane/pkg/util/dev/testing"
-	rfl "github.com/wrmsr/bane/pkg/util/reflect"
 )
 
 type MyInt int
 
 func TestConvertUnmarshal(t *testing.T) {
-	t0 := rfl.TypeOf[MyInt]()
-	t1 := rfl.TypeOf[[]MyInt]()
-	t2 := rfl.TypeOf[int]()
-	t3 := rfl.TypeOf[[]int]()
-
 	v := []MyInt{1}
 
 	mv := check.Must1(Marshal(v))
