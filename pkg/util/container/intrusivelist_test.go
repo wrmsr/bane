b7bf1d73ea9726b2e8ab04c02c4fe22edaa3bc7c
wrmsr <timwilloney@gmail.com>
2022-07-13 15:02:55 -0700

    --

--- pkg/util/container/intrusivelist_test.go
+++ pkg/util/container/intrusivelist_test.go
@@ -21,15 +21,21 @@ var (
 func TestIntrusiveList(t *testing.T) {
 	al := NewIntrusiveList[ListThing](listThingAOps)
 	bl := NewIntrusiveList[ListThing](listThingAOps)
+	_ = bl
 
 	ts := slices.Map(func(i int) *ListThing { return &ListThing{i: i} }, slices.Range1(0, 10))
 	fmt.Println(ts)
 
 	al.PushFront(ts[1])
-	bl.PushFront(ts[2])
+	//bl.PushFront(ts[2])
 
 	al.PushFront(ts[2])
-	bl.PushFront(ts[3])
+	//bl.PushFront(ts[3])
+
+	//ll := list.New()
+	//ll.PushFront(ts[1])
+	//ll.PushFront(ts[2])
+	//ll.PushFront(ts[4])
 
 	//al.PushFront(ts[4])
 	//bl.PushFront(ts[5])
