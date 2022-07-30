72aeca19e54d90cb499d48408a1493853f102090
wrmsr <timwilloney@gmail.com>
2022-07-30 13:31:31 -0700

    --

--- exp/util/tg/shapetracker_test.go
+++ exp/util/tg/shapetracker_test.go
@@ -1,6 +1,7 @@
 package tg
 
 import (
+	"fmt"
 	"testing"
 
 	tu "github.com/wrmsr/bane/pkg/util/dev/testing"
@@ -18,5 +19,49 @@ func TestReshapeAdd1s(t *testing.T) {
 	st.Reshape(Shape{1, 4, 1, 4, 1})
 	tu.AssertEqual(t, st.Contiguous(), false)
 	st.Permute(0, 3, 2, 1, 4)
+	tu.AssertEqual(t, st.Contiguous(), true)
+}
+
+func TestPermute1sSimple(t *testing.T) {
+	st := NewShapeTracker(Shape{1, 16, 9, 9})
+	st.Permute(1, 0, 2, 3)
+	tu.AssertEqual(t, st.Contiguous(), true)
+	st = NewShapeTracker(Shape{2, 16, 9, 9})
+	st.Permute(1, 0, 2, 3)
+	tu.AssertEqual(t, st.Contiguous(), false)
+}
+
+func TestRemove1sSimple(t *testing.T) {
+	st := NewShapeTracker(Shape{1, 16, 1, 1})
+	st.Reshape(Shape{16})
+	tu.AssertEqual(t, st.Contiguous(), true)
+}
+
+func TestRemove1s(t *testing.T) {
+	st := NewShapeTracker(Shape{1, 4, 1, 4, 1})
+	st.Permute(0, 3, 2, 1, 4)
+	st.Reshape(Shape{4, 4})
+	tu.AssertEqual(t, st.Contiguous(), false)
+	st.Permute(1, 0)
+	tu.AssertEqual(t, st.Contiguous(), true)
+}
+
+func TestWork(t *testing.T) {
+	st := NewShapeTracker(Shape{64, 1024, 4})
+	st.Reshape(Shape{1, 64, 128, 32})
+	st.Permute(0, 3, 1, 2)
+	st.Reshape(Shape{1, 32, 1, 64, 128})
+	st.Permute(0, 3, 4, 1, 2)
+	tu.AssertEqual(t, st.Contiguous(), true)
+}
+
+func TestWork2(t *testing.T) {
+	st := NewShapeTracker(Shape{64, 1024, 4})
+	st.Reshape(Shape{1, 64, 128, 32})
+	st.Permute(0, 3, 1, 2)
+	st.Reshape(Shape{1, 1, 32, 64, 128})
+	st.Permute(0, 3, 4, 1, 2)
+	st.Reshape(Shape{64, 1024, 4})
+	fmt.Println(st.views)
 	tu.AssertEqual(t, st.Contiguous(), true)
 }
