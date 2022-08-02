5e3a74fea5c278539651a27e16c6dbac025d531f
wrmsr <timwilloney@gmail.com>
2022-08-02 15:07:47 -0700

    --

--- pkg/util/types/coalesce.go
+++ pkg/util/types/coalesce.go
@@ -1,8 +1,19 @@
 package types
 
-func Any[T comparable](vs ...T) T {
+func Coalesce[T comparable](vs ...T) T {
 	var z T
 	for _, v := range vs {
+		if v != z {
+			return v
+		}
+	}
+	return z
+}
+
+func CoalesceR[T comparable](vs ...T) T {
+	var z T
+	for i := len(vs) - 1; i >= 0; i-- {
+		v := vs[i]
 		if v != z {
 			return v
 		}
--- pkg/util/types/utils.go
+++ pkg/util/types/utils.go
@@ -44,27 +44,6 @@ func Choose[T any](b bool, t T, f T) T {
 	return f
 }
 
-func Coalesce[T comparable](vs ...T) T {
-	var z T
-	for _, v := range vs {
-		if v != z {
-			return v
-		}
-	}
-	return z
-}
-
-func CoalesceR[T comparable](vs ...T) T {
-	var z T
-	for i := len(vs) - 1; i >= 0; i-- {
-		v := vs[i]
-		if v != z {
-			return v
-		}
-	}
-	return z
-}
-
 func Identity[T any]() func(T) T {
 	return func(v T) T { return v }
 }
