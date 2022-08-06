9912943229fc00755600ae06b52a3bb0faf72278
wrmsr <timwilloney@gmail.com>
2022-08-05 17:06:15 -0700

    --

--- exp/util/x/behave/msgs.go
+++ exp/util/x/behave/msgs.go
@@ -53,111 +53,115 @@ func (r *SimpleRegistry[K, O]) Get(key K) maps.Set[O] {
 }
 
 func (r *SimpleRegistry[K, O]) Add(obj O, key K) {
-	s := maps.ComputeDefault(r.setsByKey, key, maps.MakeSet[O])
-	// self._sets_by_key.setdefault(key, ocol.IdentitySet()).add(obj)
-	// if self._add_callback is not None:
-	// 	self._add_callback(obj, key)
+	maps.ComputeDefault(r.setsByKey, key, maps.MakeSet[O]).Add(obj)
+	if r.addCallback != nil {
+		r.addCallback(obj, key)
+	}
+}
+
+func (r *SimpleRegistry[K, O]) Remove(obj O, key K) {
+	if key != nil {
+		if s, ok := r.setsByKey[key]; ok {
+			s.Remove(obj)
+		}
+	} else {
+		for _, s := range r.setsByKey {
+			s.Remove(obj)
+		}
+	}
+}
+
+func (r *SimpleRegistry[K, O]) Clear(key K) {
+	if key != nil {
+		delete(r.setsByKey, key)
+	} else {
+		r.setsByKey = make(map[K]maps.Set[O])
+	}
 }
 
 /*
-    def remove(self, obj: O, key: K = None) -> None:
-        for key in ([key] if key is not None else list(self._sets_by_key.keys())):
-            try:
-                self._sets_by_key[key].remove(obj)
-            except KeyError:
-                pass
 
-    def clear(self, key: K = None) -> None:
-        if key is not None:
-            self._sets_by_key.clear()
-        else:
-            try:
-                del self._sets_by_key[key]
-            except KeyError:
-                pass
-}
+   class Dispatcher(Telegraph):
 
-class Dispatcher(Telegraph):
+       def __init__(self) -> None:
+           super().__init__()
 
-    def __init__(self) -> None:
-        super().__init__()
+           self._queue = queue.PriorityQueue()
+           self._listeners: SimpleRegistry[type, Telegraph] = SimpleRegistry(add_callback=self._on_add_listener)
+           self._providers: SimpleRegistry[type, TelegramProvider] = SimpleRegistry()
 
-        self._queue = queue.PriorityQueue()
-        self._listeners: SimpleRegistry[type, Telegraph] = SimpleRegistry(add_callback=self._on_add_listener)
-        self._providers: SimpleRegistry[type, TelegramProvider] = SimpleRegistry()
+       @property
+       def listeners(self) -> SimpleRegistry[type, Telegraph]:
+           return self._listeners
 
-    @property
-    def listeners(self) -> SimpleRegistry[type, Telegraph]:
-        return self._listeners
+       @property
+       def providers(self) -> SimpleRegistry[type, TelegramProvider]:
+           return self._providers
 
-    @property
-    def providers(self) -> SimpleRegistry[type, TelegramProvider]:
-        return self._providers
+       def _on_add_listener(self, receiver: Telegraph, message_type: type) -> None:
+           for provider in self._providers.get(message_type):
+               message = provider.provide_message(receiver, message_type)
+               if message is not None:
+                   self.dispatch(
+                       provider if isinstance(provider, Telegraph) else None,
+                       receiver,
+                       message,
+                   )
 
-    def _on_add_listener(self, receiver: Telegraph, message_type: type) -> None:
-        for provider in self._providers.get(message_type):
-            message = provider.provide_message(receiver, message_type)
-            if message is not None:
-                self.dispatch(
-                    provider if isinstance(provider, Telegraph) else None,
-                    receiver,
-                    message,
-                )
+       def dispatch(
+               self,
+               sender: ta.Optional[Telegraph],
+               receiver: ta.Optional[Telegraph],
+               message: T,
+               *,
+               delay: float = None,
+               needs_return_receipt: bool = False,
+       ) -> None:
+           if needs_return_receipt:
+               check.not_none(sender)
 
-    def dispatch(
-            self,
-            sender: ta.Optional[Telegraph],
-            receiver: ta.Optional[Telegraph],
-            message: T,
-            *,
-            delay: float = None,
-            needs_return_receipt: bool = False,
-    ) -> None:
-        if needs_return_receipt:
-            check.not_none(sender)
+           telegram = Telegram(
+               sender,
+               receiver,
+               message,
+               needs_return_receipt=needs_return_receipt,
+           )
 
-        telegram = Telegram(
-            sender,
-            receiver,
-            message,
-            needs_return_receipt=needs_return_receipt,
-        )
+           if not delay or delay <= 0.:
+               self._discharge(telegram)
+           else:
+               self._queue.put(Delayed(telegram, time.time() + delay))
 
-        if not delay or delay <= 0.:
-            self._discharge(telegram)
-        else:
-            self._queue.put(Delayed(telegram, time.time() + delay))
+       def _discharge(self, telegram: Telegram) -> None:
+           if telegram.receiver is not None:
+               if not telegram.receiver.handle_message(telegram):
+                   log.error(f'Message not handled: {telegram}')
 
-    def _discharge(self, telegram: Telegram) -> None:
-        if telegram.receiver is not None:
-            if not telegram.receiver.handle_message(telegram):
-                log.error(f'Message not handled: {telegram}')
+           else:
+               num_handled = 0
+               for listener in self._listeners[type(telegram.message)]:
+                   if listener.handle_message(telegram):
+                       num_handled += 1
+               if not num_handled:
+                   log.error(f'Message not handled: {telegram}')
 
-        else:
-            num_handled = 0
-            for listener in self._listeners[type(telegram.message)]:
-                if listener.handle_message(telegram):
-                    num_handled += 1
-            if not num_handled:
-                log.error(f'Message not handled: {telegram}')
+           if telegram.needs_return_receipt:
+               receipt_telegram = Telegram(
+                   self,
+                   telegram.sender,
+                   telegram,
+               )
+               self._discharge(receipt_telegram)
 
-        if telegram.needs_return_receipt:
-            receipt_telegram = Telegram(
-                self,
-                telegram.sender,
-                telegram,
-            )
-            self._discharge(receipt_telegram)
+       def handle_message(self, telegram: Telegram) -> bool:
+           return False
 
-    def handle_message(self, telegram: Telegram) -> bool:
-        return False
-
-    def update(self) -> None:
-        while not self._queue.empty():
-            cur: Delayed = self._queue.get(block=False)
-            if cur.timestamp > time.time():
-                self._queue.put(cur)
-                break
-            self._discharge(cur.telegram)
+       def update(self) -> None:
+           while not self._queue.empty():
+               cur: Delayed = self._queue.get(block=False)
+               if cur.timestamp > time.time():
+                   self._queue.put(cur)
+                   break
+               self._discharge(cur.telegram)
 
 */
