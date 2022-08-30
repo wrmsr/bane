4a5b4458057a9a164eef4de8b13a9387f669fcfa
wrmsr <timwilloney@gmail.com>
2022-08-30 16:55:08 -0700

    --

--- exp/util/codecs/compress.go
+++ exp/util/codecs/compress.go
@@ -2,6 +2,7 @@ package codecs
 
 import (
 	"bytes"
+	"compress/bzip2"
 	"compress/gzip"
 	"io"
 
@@ -28,3 +29,21 @@ func (c GzipCodec) Decode(t []byte) []byte {
 }
 
 //
+
+type Bzip2Codec struct{}
+
+var _ Codec[[]byte, []byte] = Bzip2Codec{}
+
+func (c Bzip2Codec) Encode(f []byte) []byte {
+	//b := bytes.NewBuffer(nil)
+	//w := bzip2.(b)
+	//check.Must1(w.Write(f))
+	//check.Must(w.Close())
+	//return b.Bytes()
+	panic("???")
+}
+
+func (c Bzip2Codec) Decode(t []byte) []byte {
+	r := bzip2.NewReader(bytes.NewReader(t))
+	return check.Must1(io.ReadAll(r))
+}
