e1a20a04209a11ae63baec6562df8f4fa5216e60
wrmsr <timwilloney@gmail.com>
2022-09-02 13:52:23 -0700

    --

--- exp/util/tg/bob_test.go
+++ exp/util/tg/bob_test.go
@@ -38,59 +38,34 @@ func readMnistImages(name string) nd.NdArray[float32] {
 }
 
 func readMnistLabels(name string) nd.NdArray[byte] {
-	return nd.Maker[byte]{Data: readTgFile(name)[8:]}.Make()
+	gr := check.Must1(gzip.NewReader(bytes.NewReader(readTgFile(name))))
+	b := check.Must1(io.ReadAll(gr))[8:]
+	return nd.Maker[byte]{Data: b}.Make()
 }
 
 func TestBobNet2(t *testing.T) {
-	l1t := readTgMat2("l1.json")
-	l2t := readTgMat2("l2.json")
+	l1t := NewTensor(MakeLoadBuffer(BufferOfNd(readTgMat2("l1.json"))), true)
+	l2t := NewTensor(MakeLoadBuffer(BufferOfNd(readTgMat2("l2.json"))), true)
 	samp := check.Must1(ju.UnmarshalAs[[]int](readTgFile("samp.json")))
 
 	x_train := readMnistImages("datasets/mnist/train-images-idx3-ubyte.gz")
 	y_train := readMnistLabels("datasets/mnist/train-labels-idx1-ubyte.gz")
 
-	fmt.Println(l1t.View())
-	fmt.Println(l2t.View())
-	fmt.Println(samp)
+	num_classes := 10
+	x := nd.New[float32](nd.ShapeOf(69, 784))
+	y := nd.New[float32](nd.ShapeOf(69, nd.Dim(num_classes)))
+	for i, s := range samp {
+		x.Slice(i).Assign(x_train.Slice(s).Reshape(nd.ShapeOf(784)))
+		c := y_train.Get(nd.Dim(s))
+		*y.At(nd.Dim(i), nd.Dim(c)) = -float32(num_classes)
+	}
 
-	fmt.Println(x_train.View())
-	fmt.Println(y_train.View())
+	xt := NewTensor(MakeLoadBuffer(BufferOfNd(x)), false)
+	yt := NewTensor(MakeLoadBuffer(BufferOfNd(y)), false)
 
-	//zt := xt.Dot(l1t).Relu().Dot(l2t).LogSoftmax()
-	////zt := xt.Dot(l1t).Relu().Dot(l2t)
-	//fmt.Println("====")
-	//
-	//dumpObj(zt)
-	//
-	////fmt.Println(zt.Mean(nil, false).Data().Realize())
-	////fmt.Println(zt.Mean(nil, false).Data().Realize())
-	//
-	//// garbage
-	//
-	//scc := func(out *Tensor, y_ []int) *Tensor {
-	//	check.Equal(Dim(len(y_)), out.Shape()[0])
-	//	check.Equal(out.Shape()[0], Dim(len(y_)))
-	//	num_classes := out.Shape()[len(out.Shape())-1]
-	//	yb := NewBuffer(out.Shape())
-	//	for i := Dim(0); i < Dim(len(y_)); i++ {
-	//		yb.set(float32(-1*num_classes), i, Dim(y_[i]))
-	//	}
-	//	y := NewTensor(MakeLoadBuffer(yb), false)
-	//	return out.Mul(y).Mean(nil, false)
-	//}
-	//
-	//y := make([]int, 3)
-	//for i := 0; i < 3; i++ {
-	//	y[i] = i % 3
-	//}
-	//
-	//z := scc(
-	//	zt,
-	//	y,
-	//)
-	//
-	//fmt.Println(z.Data().Realize().Nd())
-	//z.Backward()
+	out := xt.Dot(l1t).Relu().Dot(l2t).LogSoftmax()
+
+	loss := out.Mul(yt).Mean(nil, false)
+
+	loss.Backward()
 }
-
-//
