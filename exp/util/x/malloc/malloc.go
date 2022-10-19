cf1b8a2899601cbf6d8313936a3e9a96a7668db0
wrmsr <timwilloney@gmail.com>
2022-10-19 15:59:45 -0700

    --

--- exp/util/x/malloc/malloc.go
+++ exp/util/x/malloc/malloc.go
@@ -1,28 +1,19 @@
 package malloc
 
-import "unsafe"
-
 const ALIGNMENT = 16 // must be 2^N
 const ALIGNMENT_SHIFT = 4
 
 type block = uint8
 
 type pool_header struct {
-	//union {
-	//	block *_padding;
-	//	uint count;
-	//} ref;                   // number of allocated blocks
-	freeblock     *block       // pool's free list head
-	nextpool      *pool_header // next pool of this size class
-	prevpool      *pool_header // previous pool       ""
-	arenaindex    uint         // index into arenas of base adr
-	szidx         uint         // block size class index
-	nextoffset    uint         // bytes to virgin block
-	maxnextoffset uint         // largest valid nextoffset
-}
-
-func pool_header__arenaindex(p uintptr) uint {
-	return *(*uint)(unsafe.Pointer(p + 24))
+	//freeblock     *block       // pool's free list head
+	//nextpool      *pool_header // next pool of this size class
+	//prevpool      *pool_header // previous pool       ""
+	count         uint
+	arenaindex    uint // index into arenas of base adr
+	szidx         uint // block size class index
+	nextoffset    uint // bytes to virgin block
+	maxnextoffset uint // largest valid nextoffset
 }
 
 type Allocator struct {
--- exp/util/x/malloc/malloc_test.go
+++ exp/util/x/malloc/malloc_test.go
@@ -6,7 +6,12 @@ import (
 	"unsafe"
 )
 
-func TestMalloc(t *testing.T) {
+/*
+func pool_header__arenaindex(p uintptr) uint {
+	return *(*uint)(unsafe.Pointer(p + 24))
+}
+
+func main() {
 	b := new([80]byte)
 	for i := range b {
 		b[i] = byte(i)
@@ -14,4 +19,14 @@ func TestMalloc(t *testing.T) {
 	p := uintptr(unsafe.Pointer(&b[0]))
 	fmt.Println(pool_header__arenaindex(p))
 	fmt.Println(b)
+}
+*/
+
+func TestMalloc(t *testing.T) {
+	b := new([1024]byte)
+	for i := range b {
+		b[i] = byte(i)
+	}
+	p := (*pool_header)(unsafe.Pointer(&b[0]))
+	fmt.Printf("%x\n", p.count)
 }
