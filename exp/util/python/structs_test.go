aa24cbddf512ebcbf6f3fbcb048af550dbbf6337
wrmsr <timwilloney@gmail.com>
2022-10-24 18:23:35 -0700

    --

--- exp/util/python/structs_test.go
+++ exp/util/python/structs_test.go
@@ -1,94 +1,103 @@
 package python
 
-/*
-def _build_glyph_dict(*objs: ta.Sequence[T]) -> ta.Mapping[str, T]:
-    d = {}
-    for o in objs:
-        if o.glyph in objs:
-            raise KeyError(o.glyph)
-        d[o.glyph] = o
-    return d
+import (
+	"reflect"
 
-
-class Size(lang.AutoEnum):
-    NATIVE = ...
-    STANDARD = ...
-
-
-class Alignment(lang.AutoEnum):
-    NATIVE = ...
-    NONE = ...
-
-
-@dc.dataclass(frozen=True)
-class Ordering:
-    glyph: str
-    name: str
-    size: Size
-    alignment: Alignment
-
-
-ORDERINGS = _build_glyph_dict(
-    Ordering('@', 'native', Size.NATIVE, Alignment.NATIVE),
-    Ordering('=', 'native', Size.STANDARD, Alignment.NONE),
-    Ordering('<', 'little-endian', Size.STANDARD, Alignment.NONE),
-    Ordering('>', 'big-endian', Size.STANDARD, Alignment.NONE),
-    Ordering('!', 'network', Size.STANDARD, Alignment.NONE),
+	rfl "github.com/wrmsr/bane/pkg/util/reflect"
 )
 
+//
 
-class POINTER(lang.Marker):
-    pass
+type StructSize int8
 
-
-class VARIABLE(lang.Marker):
-    pass
-
-
-@dc.dataclass(frozen=True)
-class Format:
-    glyph: str
-    name: str
-    type: ta.Type[Value]
-    size: ta.Union[int, ta.Type[POINTER], ta.Type[VARIABLE]]
-    unsigned: bool = None
-
-
-FORMATS = _build_glyph_dict(
-    Format('x', 'pad', type(None), 0),
-    Format('c', 'char', bytes, 1),
-    Format('b', 'signed char', int, 1),
-    Format('B', 'unsigned char', int, 1, unsigned=True),
-    Format('?', 'bool', bool, 1),
-    Format('h', 'short', int, 2),
-    Format('H', 'unsigned short', int, 2, unsigned=True),
-    Format('i', 'int', int, 4),
-    Format('I', 'unsigned int', int, 4, unsigned=True),
-    Format('l', 'long', int, 4),
-    Format('L', 'unsigned long', int, 4, unsigned=True),
-    Format('q', 'long long', int, 8),
-    Format('Q', 'unsigned long long', int, 8, unsigned=True),
-    Format('n', 'ssize_t', int, POINTER),
-    Format('N', 'size_t', int, POINTER),
-    Format('e', 'float16', float, 2),
-    Format('f', 'float', float, 4),
-    Format('d', 'double', float, 8),
-    Format('s', 'char[]', bytes, VARIABLE),
-    Format('p', 'char[]', bytes, VARIABLE),
-    Format('P', 'void *', int, POINTER),
+const (
+	StandardSize StructSize = iota
+	NativeSize
 )
 
+//
 
-@dc.dataclass(frozen=True)
-class BitFormat(Format):
-    pass
+type StructAlignment int8
 
-
-BIT_FORMATS = _build_glyph_dict(
-    BitFormat('j', 'packed bytes', bytes, VARIABLE),
-    BitFormat('t', 'packed int', int, VARIABLE),
-    BitFormat('T', 'unsigned packed int', int, VARIABLE, unsigned=True),
-    BitFormat('X', 'pad bits', int, VARIABLE),
-    *FORMATS.values()
+const (
+	NoneAlignment StructAlignment = 0
+	NativeAlignment
 )
-*/
+
+//
+
+type StructOrdering struct {
+	Glyph rune
+	Name  string
+
+	Size      StructSize
+	Alignment StructAlignment
+}
+
+var StructOrderings = []StructOrdering{
+	{'@', "native", NativeSize, NativeAlignment},
+	{'=', "native", StandardSize, NoneAlignment},
+	{'<', "little-endian", StandardSize, NoneAlignment},
+	{'>', "big-endian", StandardSize, NoneAlignment},
+	{'!', "network", StandardSize, NoneAlignment},
+}
+
+//
+
+type StructSpecialSize int8
+
+const (
+	PointerSize StructSpecialSize = iota + 1
+	VariableSize
+)
+
+//
+
+type StructFormat struct {
+	Glyph rune
+	Name  string
+
+	Type reflect.Type
+
+	Size        int
+	SpecialSize StructSpecialSize
+
+	Unsigned bool
+}
+
+var StructFormats = []StructFormat{
+	{Glyph: 'x', Name: "pad"},
+	{Glyph: 'c', Name: "char", Type: rfl.TypeOf[byte](), Size: 1},
+	{Glyph: 'b', Name: "signed char", Type: rfl.TypeOf[int8](), Size: 1},
+	{Glyph: 'B', Name: "unsigned char", Type: rfl.TypeOf[uint8](), Size: 1, Unsigned: true},
+	{Glyph: '?', Name: "bool", Type: rfl.TypeOf[bool](), Size: 1},
+	{Glyph: 'h', Name: "short", Type: rfl.TypeOf[int16](), Size: 2},
+	{Glyph: 'H', Name: "unsigned short", Type: rfl.TypeOf[uint16](), Size: 2, Unsigned: true},
+	{Glyph: 'i', Name: "int", Type: rfl.TypeOf[int32](), Size: 4},
+	{Glyph: 'I', Name: "unsigned int", Type: rfl.TypeOf[uint32](), Size: 4, Unsigned: true},
+	{Glyph: 'l', Name: "long", Type: rfl.TypeOf[int32](), Size: 4},
+	{Glyph: 'L', Name: "unsigned long", Type: rfl.TypeOf[uint32](), Size: 4, Unsigned: true},
+	{Glyph: 'q', Name: "long long", Type: rfl.TypeOf[int64](), Size: 8},
+	{Glyph: 'Q', Name: "unsigned long long", Type: rfl.TypeOf[uint64](), Size: 8, Unsigned: true},
+	{Glyph: 'n', Name: "ssize_t", Type: rfl.TypeOf[int](), SpecialSize: PointerSize},
+	{Glyph: 'N', Name: "size_t", Type: rfl.TypeOf[int](), SpecialSize: PointerSize},
+	{Glyph: 'e', Name: "float16", Type: rfl.TypeOf[float32](), Size: 2},
+	{Glyph: 'f', Name: "float", Type: rfl.TypeOf[float32](), Size: 4},
+	{Glyph: 'd', Name: "double", Type: rfl.TypeOf[float64](), Size: 8},
+	{Glyph: 's', Name: "char[]", Type: rfl.TypeOf[[]byte](), SpecialSize: VariableSize},
+	{Glyph: 'p', Name: "char[]", Type: rfl.TypeOf[[]byte](), SpecialSize: VariableSize},
+	{Glyph: 'P', Name: "void *", Type: rfl.TypeOf[uintptr](), SpecialSize: PointerSize},
+}
+
+//
+
+type BitStructFormat struct {
+	StructFormat
+}
+
+var BitStructFormats = []BitStructFormat{
+	{StructFormat{Glyph: 'j', Name: "packed bytes", Type: rfl.TypeOf[[]byte](), SpecialSize: VariableSize}},
+	{StructFormat{Glyph: 't', Name: "packed int", Type: rfl.TypeOf[int](), SpecialSize: VariableSize}},
+	{StructFormat{Glyph: 'T', Name: "unsigned packed int", Type: rfl.TypeOf[int](), SpecialSize: VariableSize, Unsigned: true}},
+	{StructFormat{Glyph: 'X', Name: "pad bits", Type: rfl.TypeOf[int](), SpecialSize: VariableSize}},
+}
