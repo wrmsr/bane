f34aea0d0fdfac5240d7daaa6d65eb5d4d67cdeb
wrmsr <timwilloney@gmail.com>
2022-11-07 14:09:52 -0800

    --

--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.13
 	github.com/segmentio/asm v1.2.0
 	golang.org/x/arch v0.0.0-20220412001346-fc48f9fe4c15
-	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326
+	golang.org/x/exp v0.0.0-20221106115401-f9659909a136
 	golang.org/x/mod v0.6.0
 	golang.org/x/sync v0.1.0
 	golang.org/x/text v0.4.0
@@ -46,6 +46,6 @@ require (
 	github.com/kr/pretty v0.3.0 // indirect
 	github.com/rogpeppe/go-internal v1.8.1 // indirect
 	golang.org/x/crypto v0.1.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- pkg/util/encoding/json/marshal.go
+++ pkg/util/encoding/json/marshal.go
@@ -74,21 +74,47 @@ func FromMarshal(mv msh.Value) (Node, error) {
 		return False{}, nil
 
 	case msh.Int:
-		return Number{S: mv.V()}
+		return Number{S: mv.String()}, nil
 
 	case msh.Float:
+		return Number{S: mv.String()}, nil
 
 	case msh.Number:
+		return Number{S: mv.String()}, nil
 
 	case msh.String:
+		return String{S: mv.V()}, nil
 
 	case msh.Bytes:
+		panic(mv)
 
 	case msh.Array:
+		mvs := mv.V()
+		ns := make([]Node, len(mvs))
+		for i, e := range mvs {
+			n, err := FromMarshal(e)
+			if err != nil {
+				return nil, err
+			}
+			ns[i] = n
+		}
+		return Array{Values: ns}, nil
 
 	case msh.Object:
+		kvs := mv.V()
+		ps := make([]Pair, len(kvs))
+		for i, kv := range kvs {
+			k := kv.K.(msh.String).V()
+			v, err := FromMarshal(kv.V)
+			if err != nil {
+				return nil, err
+			}
+			ps[i] = bt.KvOf(k, v)
+		}
+		return Object{Pairs: ps}, nil
 
 	case msh.Any:
+		panic(mv)
 
 	}
 	panic(mv)
