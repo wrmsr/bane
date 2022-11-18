f2aa91616c078b8add5eba3c5548bd94d21e8619
wrmsr <timwilloney@gmail.com>
2022-11-18 15:12:38 -0800

    --

--- exp/util/opencl/opencl_test.go
+++ exp/util/opencl/opencl_test.go
@@ -6,4 +6,20 @@ import (
 
 func TestOpenCl(t *testing.T) {
 	GetPlatforms()
+
+	/*
+		kernel = clCreateKernel(program, "myGEMM1", &err)
+		err = clSetKernelArg(kernel, 0, sizeof(int), (void*)&M);
+		err = clSetKernelArg(kernel, 1, sizeof(int), (void*)&N);
+		err = clSetKernelArg(kernel, 2, sizeof(int), (void*)&K);
+		err = clSetKernelArg(kernel, 3, sizeof(cl_mem), (void*)&A);
+		err = clSetKernelArg(kernel, 4, sizeof(cl_mem), (void*)&B);
+		err = clSetKernelArg(kernel, 5, sizeof(cl_mem), (void*)&C);
+		const int TS = 32;
+		const size_t local[2] = { TS, TS };
+		const size_t global[2] = { M, N };
+		err = clEnqueueNDRangeKernel(queue, kernel, 2, NULL,
+		                             global, local, 0, NULL, &event);
+		err = clWaitForEvents(1, &event);
+	*/
 }
