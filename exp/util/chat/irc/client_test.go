764c6f587b60baa2775e0ff51cd6cde56e819090
wrmsr <timwilloney@gmail.com>
2023-01-18 11:09:44 -0800

    --

--- exp/util/chat/irc/client_test.go
+++ exp/util/chat/irc/client_test.go
@@ -25,112 +25,18 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"strings"
 	"testing"
+
+	"golang.org/x/sync/errgroup"
 
 	"github.com/wrmsr/bane/pkg/util/check"
 	"github.com/wrmsr/bane/pkg/util/dev/debug"
 	"github.com/wrmsr/bane/pkg/util/log"
 	"github.com/wrmsr/bane/pkg/util/sync/pools"
 )
-
-/*
-class IrcSimpleClient:
-
-    def __init__(self, username, channel, server='irc.darwin.network', port=6697):
-        super().__init__()
-
-        self.username = username
-        self.server = server
-        self.port = port
-        self.channel = channel
-
-    def connect(self):
-        ssl_context = ssl.create_default_context()
-        rawconn = socket.create_connection((self.server, self.port)).__enter__()
-        self.conn = ssl_context.wrap_socket(rawconn, server_hostname=self.server).__enter__()
-
-    def get_response(self):
-        return self.conn.recv(512).decode('utf-8')
-
-    def send_cmd(self, cmd, message):
-        command = '{} {}\r\n'.format(cmd, message).encode('utf-8')
-        self.conn.send(command)
-
-    def send_message_to_channel(self, message):
-        command = 'PRIVMSG {}'.format(self.channel)
-        message = ':' + message
-        self.send_cmd(command, message)
-
-    def join_channel(self):
-        cmd = 'JOIN'
-        channel = self.channel
-        self.send_cmd(cmd, channel)
-
-
-@pytest.mark.skip()
-def test_irc():
-    username = 'omnibus_irc_test'
-    channel = '#test'
-
-    def print_response():
-        resp = client.get_response()
-        if resp:
-            print(f'resp: {resp}')
-            msg = utils.parse_message(resp)
-            print(f'msg: {msg}')
-            # msg = resp.strip().split(':')
-            # print('< {}> {}'.format(msg[1].split('!')[0], msg[2].strip()))
-
-    cmd = ''
-    joined = False
-    client = IrcSimpleClient(username, channel)
-    client.connect()
-
-    client.send_cmd('PASS', 'smellyoulater')
-
-    client.send_cmd('NICK', username)
-    client.send_cmd('USER', '{} * * :{}'.format(username, username))
-
-    while not joined:
-        resp = client.get_response()
-        print(resp.strip())
-
-        if 'No Ident response' in resp:
-            client.send_cmd('NICK', username)
-            client.send_cmd('USER', '{} * * :{}'.format(username, username))
-
-        # we're accepted, now let's join the channel!
-        if '376' in resp:
-            client.join_channel()
-
-        # username already in use? try to use username with _
-        if '433' in resp:
-            username = '_' + username
-            client.send_cmd('NICK', username)
-            client.send_cmd('USER', '{} * * :{}'.format(username, username))
-
-        # if PING send PONG with name of the server
-        if 'PING' in resp:
-            client.send_cmd('PONG', ':' + resp.split(':')[1])
-
-        # we've joined
-        if '366' in resp:
-            joined = True
-
-    while cmd != '/quit':
-        cmd = input('< {}> '.format(username)).strip()
-        if cmd == '/quit':
-            client.send_cmd('QUIT', 'Good bye!')
-        client.send_message_to_channel(cmd)
-
-        # socket conn.receive blocks the program until a response is received
-        # to prevent blocking program execution, receive should be threaded
-        response_thread = threading.Thread(target=print_response)
-        response_thread.daemon = True
-        response_thread.start()
-*/
 
 func TestClient(t *testing.T) {
 	if !debug.IsDebuggerAttached(context.Background()) {
@@ -235,6 +141,15 @@ func TestClient(t *testing.T) {
 		}
 
 		/*
+		   def print_response():
+		        resp = client.get_response()
+		        if resp:
+		            print(f'resp: {resp}')
+		            msg = utils.parse_message(resp)
+		            print(f'msg: {msg}')
+		            # msg = resp.strip().split(':')
+		            # print('< {}> {}'.format(msg[1].split('!')[0], msg[2].strip()))
+
 		   cmd = input('< {}> '.format(username)).strip()
 		   if cmd == '/quit':
 		       client.send_cmd('QUIT', 'Good bye!')
@@ -247,6 +162,38 @@ func TestClient(t *testing.T) {
 		   response_thread.start()
 		*/
 	}
+}
+
+//
+
+type Conn struct {
+	c   net.Conn
+	br  *bufio.Reader
+	rfn func([]byte) error
+	pw  *PooledBufioWriter
+}
+
+func (c *Conn) run(ctx context.Context) error {
+	ctx, cancel := context.WithCancel(ctx)
+
+	var grp errgroup.Group
+	defer func() {
+		cancel()
+		if err := grp.Wait(); err != nil {
+			log.Error("sender failed", log.E(err))
+		}
+	}()
+
+	grp.Go(func() error { return c.runSend(ctx) })
+
+	for {
+		c.br.ReadBytes()
+
+	}
+}
+
+func (c *Conn) runSend(ctx context.Context) error {
+
 }
 
 //
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.13
 	github.com/segmentio/asm v1.2.0
 	golang.org/x/arch v0.0.0-20220412001346-fc48f9fe4c15
-	golang.org/x/exp v0.0.0-20230116083435-1de6713980de
+	golang.org/x/exp v0.0.0-20230118134722-a68e582fa157
 	golang.org/x/mod v0.7.0
 	golang.org/x/sync v0.1.0
 	golang.org/x/text v0.6.0
