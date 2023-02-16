edef1a4972cd5df7d91a927507fe1e23bd47fed8
wrmsr <timwilloney@gmail.com>
2023-02-16 14:43:08 -0800

    --

--- exp/util/consteval/scope_test.go
+++ exp/util/consteval/scope_test.go
@@ -2,6 +2,10 @@ package consteval
 
 import (
 	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
 	"testing"
 
 	"github.com/wrmsr/bane/pkg/util/check"
@@ -19,4 +23,38 @@ func TestEval(t *testing.T) {
 
 	ov := check.Must1(s.Reduce("foo"))
 	fmt.Println(check.Must1(ju.MarshalPretty(check.Must1(msh.Marshal(&ov)))))
+}
+
+func findValueSpecExpr(vs *ast.ValueSpec, n string) ast.Expr {
+	for i, vn := range vs.Names {
+		if vn.Name == n {
+			return vs.Values[i]
+		}
+	}
+	return nil
+}
+
+func TestScopeFile(t *testing.T) {
+	src := `
+package foo
+
+const foo = 10
+const bar = foo
+`
+
+	const mode = parser.AllErrors
+	var fset token.FileSet
+	fil := check.Must1(parser.ParseFile(&fset, "", src, mode))
+	_ = ast.Fprint(os.Stdout, nil, fil, nil)
+
+	s := &Scope{
+		m: make(map[string]any),
+	}
+	for n, obj := range fil.Scope.Objects {
+		av := findValueSpecExpr(obj.Decl.(*ast.ValueSpec), obj.Name)
+		v := ValueFromAst(av)
+		s.m[n] = v
+	}
+
+	fmt.Println(s.Reduce("bar"))
 }
