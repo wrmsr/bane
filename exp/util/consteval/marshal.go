40b0d457b46d902f1bf4be87f95c7453799ffeed
wrmsr <timwilloney@gmail.com>
2023-02-16 13:40:13 -0800

    --

---
+++ exp/util/consteval/marshal.go
@@ -0,0 +1,23 @@
+package consteval
+
+import msh "github.com/wrmsr/bane/pkg/util/marshal"
+
+var _ = msh.RegisterTo[Value](
+	msh.SetImplOf[Basic](),
+	msh.SetImplOf[Type](),
+	msh.SetImplOf[Struct](),
+	msh.SetImplOf[Array](),
+	msh.SetImplOf[Map](),
+	msh.SetImplOf[Ident](),
+	msh.SetImplOf[Dynamic](),
+)
+
+var _ = msh.RegisterTo[BasicKind](
+	msh.SetStringerEnumTypes(
+		IntBasic,
+		FloatBasic,
+		ImagBasic,
+		CharBasic,
+		StringBasic,
+	),
+)
---
+++ exp/util/consteval/value.go
@@ -0,0 +1,118 @@
+package consteval
+
+import "go/token"
+
+//
+
+type TypeName string
+
+//
+
+type Value interface {
+	isValue()
+}
+
+type value struct{}
+
+func (v value) isValue() {}
+
+//
+
+type BasicKind int8
+
+const (
+	IntBasic BasicKind = iota
+	FloatBasic
+	ImagBasic
+	CharBasic
+	StringBasic
+)
+
+func (k BasicKind) String() string {
+	switch k {
+	case IntBasic:
+		return "INT"
+	case FloatBasic:
+		return "FLOAT"
+	case ImagBasic:
+		return "IMAG"
+	case CharBasic:
+		return "CHAR"
+	case StringBasic:
+		return "STRING"
+	}
+	panic(k)
+}
+
+func basicKindFromAst(tok token.Token) BasicKind {
+	switch tok {
+	case token.INT:
+		return IntBasic
+	case token.FLOAT:
+		return FloatBasic
+	case token.IMAG:
+		return ImagBasic
+	case token.CHAR:
+		return CharBasic
+	case token.STRING:
+		return StringBasic
+	}
+	panic(tok)
+}
+
+type Basic struct {
+	value
+	K BasicKind
+	S string
+}
+
+//
+
+type Struct struct {
+	value
+	T TypeName
+	M map[string]Value
+}
+
+//
+
+type Type struct {
+	value
+	T TypeName
+}
+
+//
+
+type Array struct {
+	value
+	T TypeName
+	S []Value
+}
+
+//
+
+type MapEntry struct {
+	K,
+	V Value
+}
+
+type Map struct {
+	value
+	KT,
+	VT TypeName
+	S []MapEntry
+}
+
+//
+
+type Ident struct {
+	value
+	N string
+}
+
+//
+
+type Dynamic struct {
+	value
+}
+
