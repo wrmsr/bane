9594931447861a408756ebcc357dd3cd62b13d10
wrmsr <timwilloney@gmail.com>
2023-03-14 16:55:35 -0700

    --

--- pkg/util/sync/olock_test.go
+++ pkg/util/sync/olock_test.go
@@ -1,6 +1,7 @@
 package sync
 
 import (
+	"fmt"
 	"sync/atomic"
 	"testing"
 )
@@ -11,6 +12,10 @@ type Olock struct {
 	_c Lazy[chan struct{}]
 }
 
+type __unowned struct{}
+
+var _unowned = __unowned{}
+
 func (l *Olock) c() chan struct{} {
 	return l._c.Get(func() chan struct{} {
 		c := make(chan struct{}, 1)
@@ -20,13 +25,40 @@ func (l *Olock) c() chan struct{} {
 }
 
 func (l *Olock) Acquire(o any) int {
-
+	if co := l.o.Load(); co != o {
+		c := l.c()
+		_ = <-c
+		x := l.o.Load()
+		if x != nil && x != _unowned {
+			panic("oops")
+		}
+		l.o.Store(o)
+	}
+	l.d++
+	return l.d
 }
 
 func (l *Olock) Release(o any) int {
-
+	if co := l.o.Load(); co != o {
+		panic("oops")
+	}
+	l.d--
+	if l.d < 0 {
+		panic("oops")
+	}
+	if l.d < 1 {
+		l.o.Store(_unowned)
+		l.c() <- struct{}{}
+	}
+	return l.d
 }
 
 func TestOlock(t *testing.T) {
-
+	l := Olock{}
+	for i := 0; i < 3; i++ {
+		fmt.Println(l.Acquire(420))
+	}
+	for i := 0; i < 3; i++ {
+		fmt.Println(l.Release(420))
+	}
 }
