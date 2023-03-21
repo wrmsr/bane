0ee714f2fde28b8ef518e7611fa2729bf4a3ed3b
wrmsr <timwilloney@gmail.com>
2023-03-21 16:46:39 -0700

    --

--- exp/util/match/match_test.go
+++ exp/util/match/match_test.go
@@ -1,15 +1,53 @@
 package match
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"sync/atomic"
+	"testing"
+)
 
 ///
 
-type Capture struct{}
+type Capture struct {
+	seq int64
+}
+
+var captureSeq atomic.Int64
+
+func NewCapture() *Capture {
+	return &Capture{
+		seq: captureSeq.Add(1),
+	}
+}
+
+//
 
 type Captures struct {
-	Capture *Capture
-	Value   any
-	Next    *Captures
+	capture *Capture
+	value   any
+	next    *Captures
+}
+
+func MakeCaptures(c *Capture, v any) *Captures {
+	if v == nil {
+		return nil
+	}
+	return &Captures{
+		capture: c,
+		value:   v,
+	}
+}
+
+func (cs *Captures) AddAll(o *Captures) *Captures {
+	if cs == nil {
+		return o
+	}
+	return &Captures{
+		capture: cs.capture,
+		value:   cs.value,
+		next:    cs.next.AddAll(o),
+	}
 }
 
 ///
@@ -19,9 +57,8 @@ type Match interface {
 	Value() any
 	Captures() *Captures
 
-	Filter(fn func(v any) bool) Match
-	Map(fn func(v any) any) Match
-	FlatMap(fn func(v any) any) Match
+	Filter(fn func(any) bool) Match
+	Map(fn func(any) any) Match
 }
 
 //
@@ -37,16 +74,18 @@ func (m PresentMatch) Present() bool       { return true }
 func (m PresentMatch) Value() any          { return m.value }
 func (m PresentMatch) Captures() *Captures { return m.captures }
 
-func (m PresentMatch) Filter(fn func(v any) bool) Match {
-	panic("implement me")
+func (m PresentMatch) Filter(fn func(any) bool) Match {
+	if fn(m.value) {
+		return m
+	}
+	return EmptyMatch{}
 }
 
-func (m PresentMatch) Map(fn func(v any) any) Match {
-	panic("implement me")
-}
-
-func (m PresentMatch) FlatMap(fn func(v any) any) Match {
-	panic("implement me")
+func (m PresentMatch) Map(fn func(any) any) Match {
+	return PresentMatch{
+		value:    fn(m.value),
+		captures: m.captures,
+	}
 }
 
 //
@@ -55,24 +94,156 @@ type EmptyMatch struct{}
 
 var _ Match = EmptyMatch{}
 
-func (e EmptyMatch) Present() bool       { return false }
-func (e EmptyMatch) Value() any          { panic(e) }
-func (e EmptyMatch) Captures() *Captures { panic(e) }
+func (m EmptyMatch) Present() bool       { return false }
+func (m EmptyMatch) Value() any          { panic(m) }
+func (m EmptyMatch) Captures() *Captures { panic(m) }
 
-func (e EmptyMatch) Filter(fn func(v any) bool) Match {
-	panic("implement me")
+func (m EmptyMatch) Filter(fn func(any) bool) Match {
+	return m
 }
 
-func (e EmptyMatch) Map(fn func(v any) any) Match {
-	panic("implement me")
+func (m EmptyMatch) Map(fn func(any) any) Match {
+	return m
 }
 
-func (e EmptyMatch) FlatMap(fn func(v any) any) Match {
-	panic("implement me")
+///
+
+type Pattern interface {
+	Previous() Pattern
+
+	isPattern()
+}
+
+type pattern struct {
+	previous Pattern
+}
+
+func (p pattern) isPattern() {}
+
+func (p pattern) Previous() Pattern { return p.previous }
+
+//
+
+type CapturePattern struct {
+	pattern
+	capture *Capture
+}
+
+var _ Pattern = CapturePattern{}
+
+//
+
+type EqualsPattern struct {
+	pattern
+	value any
+}
+
+var _ Pattern = EqualsPattern{}
+
+//
+
+type FilterPattern struct {
+	pattern
+	fn func(any) bool
+}
+
+var _ Pattern = FilterPattern{}
+
+//
+
+type TypePattern struct {
+	pattern
+	rtype reflect.Type
+}
+
+var _ Pattern = TypePattern{}
+
+///
+
+type Property struct {
+	name string
+	fn   func(any) any
+}
+
+type PropertyPattern struct {
+	prop Property
+	pat  Pattern
+}
+
+type WithPattern struct {
+	pattern
+	propPat PropertyPattern
+}
+
+var _ Pattern = WithPattern{}
+
+///
+
+func DefaultMatch(p Pattern, v any, cs *Captures) Match {
+	if p.Previous() != nil {
+		m := DefaultMatch(p.Previous(), v, cs)
+		if !m.Present() {
+			return m
+		}
+		cs = m.Captures()
+	}
+
+	switch p := p.(type) {
+
+	case CapturePattern:
+		return PresentMatch{
+			value:    v,
+			captures: cs.AddAll(MakeCaptures(p.capture, v)),
+		}
+
+	case EqualsPattern:
+		return PresentMatch{
+			value:    v,
+			captures: cs,
+		}.Filter(func(a any) bool {
+			return reflect.DeepEqual(a, p.value)
+		})
+
+	case FilterPattern:
+		return PresentMatch{
+			value:    v,
+			captures: cs,
+		}.Filter(p.fn)
+
+	case TypePattern:
+		return PresentMatch{
+			value:    v,
+			captures: cs,
+		}.Filter(func(a any) bool {
+			return reflect.TypeOf(a) == p.rtype
+		})
+
+	case WithPattern:
+		pv := p.propPat.prop.fn(v)
+		return DefaultMatch(p.propPat.pat, pv, cs)
+
+	}
+	panic(p)
 }
 
 ///
 
 func TestMatch(t *testing.T) {
+	c := NewCapture()
+	var p Pattern
+	p = TypePattern{rtype: reflect.TypeOf(4)}
+	p = CapturePattern{pattern: pattern{previous: p}, capture: c}
+	m := DefaultMatch(p, 5, nil)
+	fmt.Printf("%#v\n", m)
+}
 
+func TestPropMatch(t *testing.T) {
+	strPat := TypePattern{rtype: reflect.TypeOf("")}
+	lenProp := Property{name: "len", fn: func(v any) any { return len(v.(string)) }}
+	len1 := PropertyPattern{prop: lenProp, pat: EqualsPattern{value: 1}}
+	withPat := WithPattern{pattern: pattern{previous: strPat}, propPat: len1}
+	m := DefaultMatch(withPat, "a", nil)
+	fmt.Printf("%#v\n", m)
+	m = DefaultMatch(withPat, "aa", nil)
+	fmt.Printf("%#v\n", m)
 }
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.13
 	github.com/segmentio/asm v1.2.0
 	golang.org/x/arch v0.0.0-20220412001346-fc48f9fe4c15
-	golang.org/x/exp v0.0.0-20230315142452-642cacee5cc0
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 	golang.org/x/mod v0.9.0
 	golang.org/x/sync v0.1.0
 	golang.org/x/sys v0.6.0
