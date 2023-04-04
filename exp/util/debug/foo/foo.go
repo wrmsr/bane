0bab7da8ab35751738be4fa26ef9561b24fd5668
wrmsr <timwilloney@gmail.com>
2023-04-03 17:47:49 -0700

    --

--- exp/util/debug/foo/foo.go
+++ exp/util/debug/foo/foo.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"debug/elf"
 	"debug/macho"
+	"encoding/binary"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"reflect"
 	"runtime"
+	"strings"
 	"unsafe"
 
 	"github.com/wrmsr/bane/pkg/util/check"
+	iou "github.com/wrmsr/bane/pkg/util/io"
 	rtu "github.com/wrmsr/bane/pkg/util/runtime"
 )
 
@@ -41,6 +47,176 @@ func (f machoExeFile) ForEachSym(fn func(s ExeSym) bool) (bool, error) {
 			Addr: s.Value,
 		}) {
 			return false, nil
+		}
+	}
+	return true, nil
+}
+
+//
+
+type machoExeFile2 struct {
+	name string
+}
+
+var _ ExeFile = machoExeFile2{}
+
+const (
+	machoFileHeaderSize32 = 7 * 4
+	machoFileHeaderSize64 = 8 * 4
+)
+
+func cstring(b []byte) string {
+	i := bytes.IndexByte(b, 0)
+	if i == -1 {
+		i = len(b)
+	}
+	return string(b[0:i])
+}
+
+func machoParseSymtab(f *macho.File, symdat, strtab, cmddat []byte, hdr *macho.SymtabCmd, offset int64) (*macho.Symtab, error) {
+	bo := f.ByteOrder
+	c := iou.SafeSliceCap((*macho.Symbol)(nil), uint64(hdr.Nsyms))
+	if c < 0 {
+		return nil, errors.New("too many symbols")
+	}
+	symtab := make([]macho.Symbol, 0, c)
+	b := bytes.NewReader(symdat)
+	for i := 0; i < int(hdr.Nsyms); i++ {
+		var n macho.Nlist64
+		if f.Magic == macho.Magic64 {
+			if err := binary.Read(b, bo, &n); err != nil {
+				return nil, err
+			}
+		} else {
+			var n32 macho.Nlist32
+			if err := binary.Read(b, bo, &n32); err != nil {
+				return nil, err
+			}
+			n.Name = n32.Name
+			n.Type = n32.Type
+			n.Sect = n32.Sect
+			n.Desc = n32.Desc
+			n.Value = uint64(n32.Value)
+		}
+		if n.Name >= uint32(len(strtab)) {
+			return nil, errors.New("invalid name in symbol table")
+		}
+		// We add "_" to Go symbols. Strip it here. See issue 33808.
+		name := cstring(strtab[n.Name:])
+		if strings.Contains(name, ".") && name[0] == '_' {
+			name = name[1:]
+		}
+		symtab = append(symtab, macho.Symbol{
+			Name:  name,
+			Type:  n.Type,
+			Sect:  n.Sect,
+			Desc:  n.Desc,
+			Value: n.Value,
+		})
+	}
+	st := new(macho.Symtab)
+	st.LoadBytes = macho.LoadBytes(cmddat)
+	st.Syms = symtab
+	return st, nil
+}
+
+func (f2 machoExeFile2) ForEachSym(fn func(s ExeSym) bool) (bool, error) {
+	r, err := os.Open(f2.name)
+	if err != nil {
+		return false, err
+	}
+	sr := io.NewSectionReader(r, 0, 1<<63-1)
+
+	var f macho.File
+
+	// Read and decode Mach magic to determine byte order, size.
+	// Magic32 and Magic64 differ only in the bottom bit.
+	var ident [4]byte
+	if _, err := r.ReadAt(ident[0:], 0); err != nil {
+		return false, err
+	}
+	be := binary.BigEndian.Uint32(ident[0:])
+	le := binary.LittleEndian.Uint32(ident[0:])
+	switch macho.Magic32 &^ 1 {
+	case be &^ 1:
+		f.ByteOrder = binary.BigEndian
+		f.Magic = be
+	case le &^ 1:
+		f.ByteOrder = binary.LittleEndian
+		f.Magic = le
+	default:
+		return false, errors.New("invalid magic number")
+	}
+
+	// Read entire file header.
+	if err := binary.Read(sr, f.ByteOrder, &f.FileHeader); err != nil {
+		return false, err
+	}
+
+	// Then load commands.
+	offset := int64(machoFileHeaderSize32)
+	if f.Magic == macho.Magic64 {
+		offset = machoFileHeaderSize64
+	}
+	dat, err := iou.SafeReadDataAt(r, uint64(f.Cmdsz), offset)
+	if err != nil {
+		return false, err
+	}
+	c := iou.SafeSliceCap((*macho.Load)(nil), uint64(f.Ncmd))
+	if c < 0 {
+		return false, errors.New("too many load commands")
+	}
+	f.Loads = make([]macho.Load, 0, c)
+	bo := f.ByteOrder
+	for i := uint32(0); i < f.Ncmd; i++ {
+		// Each load command begins with uint32 command and length.
+		if len(dat) < 8 {
+			return false, errors.New("command block too small")
+		}
+		cmd, siz := macho.LoadCmd(bo.Uint32(dat[0:4])), bo.Uint32(dat[4:8])
+		if siz < 8 || siz > uint32(len(dat)) {
+			return false, errors.New("invalid command block size")
+		}
+		var cmddat []byte
+		cmddat, dat = dat[0:siz], dat[siz:]
+		offset += int64(siz)
+		var s *macho.Segment
+		switch cmd {
+
+		case macho.LoadCmdSymtab:
+			var hdr macho.SymtabCmd
+			b := bytes.NewReader(cmddat)
+			if err := binary.Read(b, bo, &hdr); err != nil {
+				return false, err
+			}
+			strtab := make([]byte, hdr.Strsize)
+			if _, err := r.ReadAt(strtab, int64(hdr.Stroff)); err != nil {
+				return false, err
+			}
+			var symsz int
+			if f.Magic == macho.Magic64 {
+				symsz = 16
+			} else {
+				symsz = 12
+			}
+			symdat, err := iou.SafeReadDataAt(r, uint64(hdr.Nsyms)*uint64(symsz), int64(hdr.Symoff))
+			if err != nil {
+				return false, err
+			}
+			st, err := machoParseSymtab(&f, symdat, strtab, cmddat, &hdr, offset)
+			if err != nil {
+				return false, err
+			}
+			f.Loads = append(f.Loads, st)
+			f.Symtab = st
+		}
+		if s != nil {
+			if int64(s.Offset) < 0 {
+				return false, errors.New("invalid section offset")
+			}
+			if int64(s.Filesz) < 0 {
+				return false, errors.New("invalid section file size")
+			}
 		}
 	}
 	return true, nil
@@ -107,13 +283,29 @@ var marker = 0
 
 //
 
+func debugWait() {
+	log.Println(os.Getpid())
+	bufio.NewScanner(os.Stdin).Scan()
+}
+
 func main() {
+	debugWait()
+
 	pn := rtu.GetCaller(0)
 	pu := reflect.ValueOf(&marker).Pointer()
 
-	f, clo := check.Must2(OpenExeFile(os.Args[0]))
+	name := os.Args[0]
+
+	var f ExeFile
+	var clo Closer
+
+	//f, clo = check.Must2(OpenExeFile(name))
+	f = machoExeFile2{name: name}
+
 	defer func() {
-		clo()
+		if clo != nil {
+			check.Must(clo())
+		}
 	}()
 
 	var ofs uintptr
@@ -133,6 +325,5 @@ func main() {
 	var std **log.Logger = (**log.Logger)(unsafe.Pointer(lp + ofs))
 	(*std).Println("hi")
 
-	log.Println(os.Getpid())
-	bufio.NewScanner(os.Stdin).Scan()
+	debugWait()
 }
---
+++ pkg/util/io/safe.go
@@ -0,0 +1,118 @@
+// Copyright 2022 The Go Authors. All rights reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+package io
+
+import (
+	"io"
+	"reflect"
+)
+
+// chunk is an arbitrary limit on how much memory we are willing to allocate without concern.
+const safeChunk = 10 << 20 // 10M
+
+// ReadData reads n bytes from the input stream, but avoids allocating all n bytes if n is large. This avoids crashing
+// the program by allocating all n bytes in cases where n is incorrect.
+//
+// The error is io.EOF only if no bytes were read. If an io.EOF happens after reading some but not all the bytes,
+// ReadData returns io.ErrUnexpectedEOF.
+func SafeReadData(r io.Reader, n uint64) ([]byte, error) {
+	if int64(n) < 0 || n != uint64(int(n)) {
+		// n is too large to fit in int, so we can't allocate a buffer large enough. Treat this as a read failure.
+		return nil, io.ErrUnexpectedEOF
+	}
+
+	if n < safeChunk {
+		buf := make([]byte, n)
+		_, err := io.ReadFull(r, buf)
+		if err != nil {
+			return nil, err
+		}
+		return buf, nil
+	}
+
+	var buf []byte
+	buf1 := make([]byte, safeChunk)
+	for n > 0 {
+		next := n
+		if next > safeChunk {
+			next = safeChunk
+		}
+		_, err := io.ReadFull(r, buf1[:next])
+		if err != nil {
+			if len(buf) > 0 && err == io.EOF {
+				err = io.ErrUnexpectedEOF
+			}
+			return nil, err
+		}
+		buf = append(buf, buf1[:next]...)
+		n -= next
+	}
+	return buf, nil
+}
+
+// ReadDataAt reads n bytes from the input stream at off, but avoids allocating all n bytes if n is large. This avoids
+// crashing the program by allocating all n bytes in cases where n is incorrect.
+func SafeReadDataAt(r io.ReaderAt, n uint64, off int64) ([]byte, error) {
+	if int64(n) < 0 || n != uint64(int(n)) {
+		// n is too large to fit in int, so we can't allocate a buffer large enough. Treat this as a read failure.
+		return nil, io.ErrUnexpectedEOF
+	}
+
+	if n < safeChunk {
+		buf := make([]byte, n)
+		_, err := r.ReadAt(buf, off)
+		if err != nil {
+			// io.SectionReader can return EOF for n == 0, but for our purposes that is a success.
+			if err != io.EOF || n > 0 {
+				return nil, err
+			}
+		}
+		return buf, nil
+	}
+
+	var buf []byte
+	buf1 := make([]byte, safeChunk)
+	for n > 0 {
+		next := n
+		if next > safeChunk {
+			next = safeChunk
+		}
+		_, err := r.ReadAt(buf1[:next], off)
+		if err != nil {
+			return nil, err
+		}
+		buf = append(buf, buf1[:next]...)
+		n -= next
+		off += int64(next)
+	}
+	return buf, nil
+}
+
+// SliceCap returns the capacity to use when allocating a slice. After the slice is allocated with the capacity, it
+// should be built using append. This will avoid allocating too much memory if the capacity is large and incorrect.
+//
+// A negative result means that the value is always too big.
+//
+// The element type is described by passing a pointer to a value of that type. This would ideally use generics, but this
+// code is built with the bootstrap compiler which need not support generics. We use a pointer so that we can handle
+// slices of interface type.
+func SafeSliceCap(v any, c uint64) int {
+	if int64(c) < 0 || c != uint64(int(c)) {
+		return -1
+	}
+	typ := reflect.TypeOf(v)
+	if typ.Kind() != reflect.Ptr {
+		panic("SliceCap called with non-pointer type")
+	}
+	size := uint64(typ.Elem().Size())
+	if size > 0 && c > (1<<64-1)/size {
+		return -1
+	}
+	if c*size > safeChunk {
+		c = safeChunk / size
+		if c == 0 {
+			c = 1
+		}
+	}
+	return int(c)
+}
