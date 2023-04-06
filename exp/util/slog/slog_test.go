8088ee349840ea1cd4c8e38a10dacef081050cdc
wrmsr <timwilloney@gmail.com>
2023-04-06 12:30:43 -0700

    --

--- exp/util/slog/slog_test.go
+++ exp/util/slog/slog_test.go
@@ -2,31 +2,48 @@ package slog
 
 import (
 	"context"
+	"net"
+	"os"
 	"testing"
 
 	"golang.org/x/exp/slog"
 )
 
-type colorHandler struct{}
+type colorHandler struct {
+	level slog.Level
+}
+
+func (h *colorHandler) clone() *colorHandler {
+	return &colorHandler{
+		level: h.level,
+	}
+}
 
 var _ slog.Handler = &colorHandler{}
 
-func (h colorHandler) Enabled(ctx context.Context, level slog.Level) bool {
+func (h *colorHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	return level >= h.level
+}
+
+func (h *colorHandler) Handle(ctx context.Context, record slog.Record) error {
 	panic("implement me")
 }
 
-func (h colorHandler) Handle(ctx context.Context, record slog.Record) error {
+func (h *colorHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
 	panic("implement me")
 }
 
-func (h colorHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	panic("implement me")
-}
-
-func (h colorHandler) WithGroup(name string) slog.Handler {
+func (h *colorHandler) WithGroup(name string) slog.Handler {
 	panic("implement me")
 }
 
 func TestSlog(t *testing.T) {
-
+	slog.SetDefault(slog.New(
+		//slog.NewTextHandler(os.Stderr),
+		slog.NewJSONHandler(os.Stderr),
+	))
+	slog.Info("hello", "name", "Al")
+	slog.Error("oops", "err", net.ErrClosed, "status", 500)
+	slog.LogAttrs(nil, slog.LevelError, "oops",
+		slog.Any("err", net.ErrClosed), slog.Int("status", 500))
 }
