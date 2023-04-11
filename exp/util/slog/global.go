d16342e9a41e7f35fb7debb80b7376201a87a05d
wrmsr <timwilloney@gmail.com>
2023-04-11 11:47:57 -0700

    --

--- exp/util/slog/global.go
+++ exp/util/slog/global.go
@@ -2,9 +2,10 @@ package slog
 
 import (
 	"context"
+	"fmt"
 )
 
-// logger.go
+//
 
 func Log(ctx context.Context, level Level, msg string, args ...any) {
 	DefaultCtx(ctx).log(level, ActionNone, msg, args...)
@@ -45,4 +46,88 @@ func Error(msg string, args ...any) {
 
 func ErrorCtx(ctx context.Context, msg string, args ...any) {
 	DefaultCtx(ctx).log(LevelError, ActionNone, msg, args...)
+}
+
+//
+
+//func Panic(msg any, args ...Arg) {
+//	if msg, ok := msg.(string); ok {
+//		Global().Log(PanicLevel, msg, args...)
+//		return
+//	}
+//	Global().Log(PanicLevel, fmt.Sprint(msg), args...)
+//}
+
+//func Fatal(msg any, args ...Arg) {
+//	if msg, ok := msg.(string); ok {
+//		Global().Log(FatalLevel, msg, args...)
+//		return
+//	}
+//	Global().Log(FatalLevel, fmt.Sprint(msg), args...)
+//}
+
+//func IfError(err error, args ...Arg) {
+//	if err != nil {
+//		Global().Log(ErrorLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//func IfPanic(err error, args ...Arg) {
+//	if err != nil {
+//		Global().Log(PanicLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//func IfFatal(err error, args ...Arg) {
+//	if err != nil {
+//		Global().Log(FatalLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//func OrError(fn func() error, args ...Arg) {
+//	if err := fn(); err != nil {
+//		Global().Log(ErrorLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//func OrPanic(fn func() error, args ...Arg) {
+//	if err := fn(); err != nil {
+//		Global().Log(PanicLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//func OrFatal(fn func() error, args ...Arg) {
+//	if err := fn(); err != nil {
+//		Global().Log(FatalLevel, "error", append(args, E(err))...)
+//	}
+//}
+
+//
+
+func Print(v ...any) {
+	Default().log(LevelInfo, ActionNone, fmt.Sprint(v...))
+}
+
+func Printf(format string, v ...any) {
+	Default().log(LevelInfo, ActionNone, fmt.Sprintf(format, v...))
+}
+
+func Println(v ...any) {
+	Default().log(LevelInfo, ActionNone, fmt.Sprintln(v...))
+}
+
+func Fatalf(format string, v ...any) {
+	Default().log(LevelError, ActionFatal, fmt.Sprintf(format, v...))
+}
+
+func Fatalln(v ...any) {
+	Default().log(LevelError, ActionFatal, fmt.Sprintln(v...))
+}
+
+func Panicf(format string, v ...any) {
+	Default().log(LevelError, ActionPanic, fmt.Sprintf(format, v...))
+}
+
+func Panicln(v ...any) {
+	Default().log(LevelError, ActionPanic, fmt.Sprintln(v...))
 }
