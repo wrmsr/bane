3daef2586c0d97a0063b3f9f13dd72791830ab1c
wrmsr <timwilloney@gmail.com>
2023-04-11 14:25:40 -0700

    --

--- exp/util/slog/format/text.go
+++ exp/util/slog/format/text.go
@@ -1,6 +1,6 @@
 // Copyright 2022 The Go Authors. All rights reserved. Use of this source code is governed by a BSD-style license that
 // can be found in the LICENSE file.
-package formatters
+package format
 
 import (
 	"encoding"
@@ -134,6 +134,7 @@ func byteSlice(a any) ([]byte, bool) {
 	}
 	return nil, false
 }
+
 //
 
 func needsQuoting(s string) bool {
